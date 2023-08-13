c14f88eb869455ddc6a1622d00e8e3f53fe5b4ae
Lufi-SAN <88794163+Lufi-SAN@users.noreply.github.com>
2023-08-13 19:26:14 +0100

    Refactored to have buttons provide user choice

--- myscript.js
+++ myscript.js
@@ -1,77 +1,63 @@
-let aroc = ["rock", "paper", "scissors"];
-let playerSelection;
-let rounds = [];
+const computerChoiceArray = ["rock", "paper", "scissors"];
+const playerWinMessage = "You win";
+const computerWinMessage = "Computer wins";
+const playerWinCollectionArray = [];
+const computerWinCollectionArray = [];
 
-function player_Selection() {
-  //prompt player choice
-  let p = prompt("Enter");
-  while (p == null || p == " ") {
-    p = prompt("Enter");
-  }
-  return validatePlayerChoice(p).toLowerCase();
-}
+let button = document.querySelectorAll("button");
+//console.log(button);
+const div = document.querySelector(".displayResults");
 
-function computer_Selection() {
-  //randomly get computer choice
-  return aroc[Math.floor(Math.random() * 3)];
-}
+button.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    //console.log(e.target.id);
+    div.textContent = "";
+    if (computerChoiceArray.includes(e.target.id)) {
+      playRound(e.target.id);
+    }
 
-let validatePlayerChoice = function (p) {
-  //let playerSelection = player_Selection();
-  while (!aroc.includes(p)) {
-    p = prompt("Wrong value. Choose from array again");
-  }
-  return p;
-};
-
-function playRound(playerSelection, computerSelection) {
-  // your code here!
-  playerSelection = player_Selection();
-  computerSelection = computer_Selection();
-  if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "scissors" && computerSelection === "paper") ||
-    (playerSelection === "paper" && computerSelection === "rock")
-  ) {
-    rounds.push("P");
-    alert("YOu win");
-  } else if (playerSelection === computerSelection) {
-    alert("A draw you will go again");
-    drawfunction();
-    return;
-  } else {
-    rounds.push("C");
-    alert("Computer wins");
-  }
-}
-
-function drawfunction() {
-  playRound();
-}
-
-function playGame() {
-  for (let gameC = 0; gameC < 5; gameC++) {
-    playRound();
-  }
-
-  alert(rounds);
-  displayWinner();
-}
+    if (
+      playerWinCollectionArray.length + computerWinCollectionArray.length ===
+      5
+    ) {
+      displayWinner();
+    }
+  });
+});
 
 function displayWinner() {
-  let cInstances = [];
-  rounds.filter((age) => {
-    if (age === "C") cInstances.push("C");
-  });
-
-  if (cInstances.length < 3) {
-    alert("You win the whole shabang");
+  if (playerWinCollectionArray.length > computerWinCollectionArray.length) {
+    div.textContent = "You win the whole shabang";
   } else {
-    alert("Computer wins the whole shabang");
+    // const div = document.querySelector(".displayResults");
+    div.textContent = "Whomp, whomp.";
+  }
+  playerWinCollectionArray.length = 0;
+  computerWinCollectionArray.length = 0;
+  console.log(playerWinCollectionArray.length);
+  console.log(computerWinCollectionArray.length);
+}
+
+function playRound(playerChoice) {
+  let computerChoice = selectComputerValue();
+  console.log(playerChoice);
+  console.log(computerChoice);
+  if (
+    (playerChoice === "rock" && computerChoice === "scissors") ||
+    (playerChoice === "scissors" && computerChoice === "paper") ||
+    (playerChoice === "paper" && computerChoice === "rock")
+  ) {
+    console.log(playerWinMessage);
+    playerWinCollectionArray.push("P");
+  } else if (playerChoice === computerChoice) {
+    alert("Draw, you're going again");
+    return;
+  } else {
+    console.log(computerWinMessage);
+    computerWinCollectionArray.push("C");
   }
 }
 
-playGame();
-/**const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));*/
+function selectComputerValue() {
+  return computerChoiceArray[Math.floor(Math.random() * 3)];
+}
